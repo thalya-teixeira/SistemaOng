8de750ec5b27e8a26bac11b64c4350be2ef3c9fc
Thalya Teixeira <thalyateixeira9@hotmail.com>
2022-09-25 05:54:12 -0300

    inserindo métodos na classe controleadocao

--- OngClubeDosAumigos/Controle_Adocao.cs
+++ OngClubeDosAumigos/Controle_Adocao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,18 +11,56 @@ namespace OngClubeDosAumigos
     {
         public int Num_Chip { get; set; }
         public string CPF { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime DataAdocao { get; set; }
 
-        public Controle_Adocao(int num_Chip, string cPF, DateTime date)
+        public Controle_Adocao(int num_Chip, string cpf, DateTime date)
         {
             Num_Chip = num_Chip;
-            CPF = cPF;
-            Date = date;
+            CPF = cpf;
+            DataAdocao = date;
         }
 
         public Controle_Adocao()
         {
 
         }
+
+        public void CadastroAdocao()
+        {
+            Console.Write("Informe o número de identificação do animal: ");
+            Num_Chip = int.Parse(Console.ReadLine());
+            Console.Write("Informe o númedo do CPF: ");
+            CPF = Console.ReadLine();
+            DataAdocao = DateTime.Now;
+        }
+        #region Visualizar CadastroAdocao
+        public void VisualizarAdotante()
+        {
+            ConexaoBanco conn = new ConexaoBanco();
+            SqlConnection conexaosql = new SqlConnection(conn.Caminho());
+            //ver se está chamando
+            //Console.WriteLine(conn.Caminho());
+            conexaosql.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT Num_Chip, CPF, DataAdocao FROM Controle_Adocao";
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}", reader.GetInt32(0));
+                    Console.WriteLine("{0}", reader.GetString(1));
+                    Console.WriteLine("{0}", reader.GetString(2));
+              
+                }
+                Console.WriteLine("\nPressione qualquer tecla para continuar...");
+                Console.ReadKey();
+            }
+            conexaosql.Close();
+
+        }
+        #endregion
     }
 }
