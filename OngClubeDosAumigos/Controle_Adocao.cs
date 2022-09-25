9bcdbe9e5980fbc5bc82e09552f78576a9913cc1
Thalya Teixeira <thalyateixeira9@hotmail.com>
2022-09-25 05:41:18 -0300

    criando classe controle_adocao

---
+++ OngClubeDosAumigos/Controle_Adocao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngClubeDosAumigos
+{
+    internal class Controle_Adocao
+    {
+        public int Num_Chip { get; set; }
+        public string CPF { get; set; }
+        public DateTime Date { get; set; }
+
+        public Controle_Adocao(int num_Chip, string cPF, DateTime date)
+        {
+            Num_Chip = num_Chip;
+            CPF = cPF;
+            Date = date;
+        }
+
+        public Controle_Adocao()
+        {
+
+        }
+    }
+}
