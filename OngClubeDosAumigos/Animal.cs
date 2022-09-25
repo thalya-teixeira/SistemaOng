c50255cd07e942b0adff292ee5066dd6dd8c6fea
Thalya Teixeira <thalyateixeira9@hotmail.com>
2022-09-25 05:47:10 -0300

    adicionando metodo update

--- OngClubeDosAumigos/Animal.cs
+++ OngClubeDosAumigos/Animal.cs
@@ -109,5 +109,23 @@ namespace OngClubeDosAumigos
 
         }
         #endregion
+
+        #region Editar Adotante
+        public void UpdateAdotante()
+        {
+            ConexaoBanco conn = new ConexaoBanco();
+            SqlConnection conexaosql = new SqlConnection(conn.Caminho());
+            //ver se está chamando
+            //Console.WriteLine(conn.Caminho());
+            conexaosql.Open();
+
+            SqlCommand cmd = new SqlCommand();
+
+            //fazer um switch para escolher o dado a partir do cpf 
+
+            conexaosql.Close();
+        }
+        #endregion
+
     }
 }
